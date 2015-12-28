5c167a742f8461656d8e2d0b899e383706754b5c
Dave Caulton <dcaulton@gmail.com>
2015-12-28 01:33:39 +0000

    now a url works for picam_still

--- thermal/app.py
+++ thermal/app.py
@@ -1,4 +1,8 @@
+import datetime
+
 from flask import Flask
+import picamera
+import os
 
 
 app = Flask(__name__)
@@ -7,6 +11,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World'
+
+@app.route('/picam_still')
+def picam_still():
+    with picamera.PiCamera() as camera:
+        datetime_str = str(datetime.datetime.today())
+        pic_path = os.path.join('/home/pi', 'Pictures', datetime_str+'-picam_pic.jpg')
+        camera.capture(pic_path)
+        return pic_path
 
 
 if __name__ == '__main__':
