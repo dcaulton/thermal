7d486913165b7f587ac5c1654cea17c1994262d5
Dave Caulton <dcaulton@gmail.com>
2015-12-29 22:27:11 +0000

    Save picture files with the guid as the filename prefix

--- thermal/app.py
+++ thermal/app.py
@@ -61,39 +61,35 @@ def set_settings():
         return Response(json.dumps(settings), status=200, mimetype='application/json')
 
 @app.route('/picam_still')
-def picam_still():
+def picam_still(the_uuid=uuid.uuid4()):
     with picamera.PiCamera() as camera:
-        datetime_str = str(datetime.datetime.today())
-        pic_path = os.path.join('/home/pi', 'Pictures', datetime_str+'-picam_pic.jpg')
+        pic_path = os.path.join('/home/pi', 'Pictures', str(the_uuid)+'-picam_pic.jpg')
         camera.capture(pic_path)
-        pic_id = str(uuid.uuid4())
         pic_dict = {'type': 'picture',
                     'camera_type': 'picam',
                     'group_id': str(current_group_id),
                     'uri': 'file://strangefruit4'+pic_path,
                     'created': str(datetime.datetime.now())
                    }
-        g.db[pic_id] = pic_dict
+        g.db[str(the_uuid)] = pic_dict
         return Response(json.dumps(pic_dict), status=200, mimetype='application/json')
 
 @app.route('/thermal_still')
-def thermal_still():
+def thermal_still(the_uuid=uuid.uuid4()):
     try:
         with Lepton("/dev/spidev0.1") as l:
             a,_ = l.capture()
             cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX) 
             np.right_shift(a, 8, a) 
-            datetime_str = str(datetime.datetime.today())
-            pic_path = os.path.join('/home/pi', 'Pictures', datetime_str+'-thermal_pic.jpg')
+            pic_path = os.path.join('/home/pi', 'Pictures', str(the_uuid)+'-thermal_pic.jpg')
             cv2.imwrite(pic_path, np.uint8(a)) 
-            pic_id = str(uuid.uuid4())
             pic_dict = {'type': 'picture',
                         'camera_type': 'thermal',
                         'group_id': str(current_group_id),
                         'uri': 'file://strangefruit4'+pic_path,
                         'created': str(datetime.datetime.now())
                        }
-            g.db[pic_id] = pic_dict
+            g.db[str(the_uuid)] = pic_dict
             return Response(json.dumps(pic_dict), status=200, mimetype='application/json')
     except Exception as e:
         e = sys.exc_info()[0]
