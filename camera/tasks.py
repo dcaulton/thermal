430062d4a84b2857f793576b3e2bd7f03134e912
Dave Caulton <dcaulton@gmail.com>
2016-01-07 15:06:38 -0600

    Support scheduling delayed and repeated tasks for thermal_still and picam_still

--- camera/tasks.py
+++ camera/tasks.py
@@ -14,95 +14,116 @@ def take_picam_still_chained(_, snap_id, group_id, normal_exposure_pic_id, long_
     #  sequence with the original chain of tasks we belong to
     camera.services.take_picam_still(snap_id, group_id, normal_exposure_pic_id, long_exposure_pic_id)
 
-def take_picam_still(snap_id, group_id, delay=0):
-    normal_exposure_pic_id = uuid.uuid4()
-    long_exposure_pic_id = uuid.uuid4()
-    task = picam_still_task.apply_async(
-        kwargs={
-            'snap_id': snap_id,
-            'group_id': group_id,
-            'normal_exposure_pic_id': normal_exposure_pic_id,
-            'long_exposure_pic_id': long_exposure_pic_id
-        },
-        countdown=delay
-    )
+def take_picam_still(snap_id, group_id, delay=0, repeat=0):
+    normal_exposure_pic_ids = []
+    long_exposure_pic_ids = []
+    snap_ids = []
+    for i in [x*delay for x in range(1,repeat+2)]:
+        normal_exposure_pic_id = uuid.uuid4()
+        long_exposure_pic_id = uuid.uuid4()
+        task = picam_still_task.apply_async(
+            kwargs={
+                'snap_id': snap_id,
+                'group_id': group_id,
+                'normal_exposure_pic_id': normal_exposure_pic_id,
+                'long_exposure_pic_id': long_exposure_pic_id
+            },
+            countdown=i
+        )
+        normal_exposure_pic_ids.append(str(normal_exposure_pic_id))
+        long_exposure_pic_ids.append(str(long_exposure_pic_id))
+        snap_ids.append(str(snap_id))
+        snap_id = uuid.uuid4()
     return {
-        'snap_id': str(snap_id),
+        'snap_ids': str(snap_ids),
         'group_id': str(group_id),
-        'normal_exposure_pic_id': str(normal_exposure_pic_id),
-        'long_exposure_pic_id': str(long_exposure_pic_id)
+        'normal_exposure_pic_ids': str(normal_exposure_pic_ids),
+        'long_exposure_pic_ids': str(long_exposure_pic_ids)
     }
 
 @celery.task
 def picam_still_task(snap_id, group_id, normal_exposure_pic_id, long_exposure_pic_id):
     camera.services.take_picam_still(snap_id, group_id, normal_exposure_pic_id, long_exposure_pic_id)
 
-def take_thermal_still(snap_id, group_id, delay=0, scale_image=True):
-    pic_id = uuid.uuid4()
-    scaled_pic_id = uuid.uuid4()
-    return_dict = {
-        'snap_id': str(snap_id),
-        'group_id': str(group_id),
-        'pic_id': str(pic_id)
-    }
+def take_thermal_still(snap_id, group_id, delay=0, repeat=0, scale_image=True):
+    pic_ids = []
+    scaled_pic_ids = []
+    snap_ids = []
     if scale_image:
-        chain(
-            thermal_still_task.s(
-                snap_id=snap_id,
-                group_id=group_id,
-                pic_id=pic_id
-            ),
-            scale_image_chained.s(
-                img_id_in=pic_id,
-                img_id_out=scaled_pic_id
-            )
-        ).apply_async(countdown=delay)
-        return_dict['scaled_pic_id'] = str(scaled_pic_id)
+        for i in [x*delay for x in range(1,repeat+2)]:
+            pic_id = uuid.uuid4()
+            scaled_pic_id = uuid.uuid4()
+            chain(
+                thermal_still_task.s(
+                    snap_id=snap_id,
+                    group_id=group_id,
+                    pic_id=pic_id
+                ),
+                scale_image_chained.s(
+                    img_id_in=pic_id,
+                    img_id_out=scaled_pic_id
+                )
+            ).apply_async(countdown=i)
+            pic_ids.append(str(pic_id))
+            snap_ids.append(str(snap_id))
+            scaled_pic_ids.append(str(scaled_pic_id))
+            snap_id = uuid.uuid4()
     else:
-        thermal_still_task.apply_async(
-            kwargs={
-                'snap_id': snap_id,
-                'group_id': group_id,
-                'pic_id': pic_id
-            },
-            countdown=delay
-        )
-    return return_dict
+        for i in [x*delay for x in range(1,repeat+2)]:
+            pic_id = uuid.uuid4()
+            thermal_still_task.apply_async(
+                kwargs={
+                    'snap_id': snap_id,
+                    'group_id': group_id,
+                    'pic_id': pic_id
+                },
+                countdown=i
+            )
+            pic_ids.append(str(pic_id))
+            snap_ids.append(str(snap_id))
+            snap_id = uuid.uuid4()
+    return {
+        'pic_ids': str(pic_ids),
+        'scaled_pic_ids': str(scaled_pic_ids),
+        'snap_ids': str(snap_ids),
+        'group_id': str(group_id)
+    }
 
 @celery.task
 def thermal_still_task(snap_id, group_id, pic_id):
     camera.services.take_thermal_still(snap_id, group_id, pic_id)
 
-def both_still_chain(snap_id, group_id, delay=0):
+def both_still_chain(snap_id, group_id, delay=0, repeat=0):
     thermal_pic_id = uuid.uuid4()
     normal_exposure_picam_pic_id = uuid.uuid4()
     long_exposure_picam_pic_id = uuid.uuid4()
     scaled_pic_id = uuid.uuid4()
     merged_pic_id = uuid.uuid4()
 
-    chain(
-        thermal_still_task.s(
-            snap_id=snap_id,
-            group_id=group_id,
-            pic_id=thermal_pic_id
-        ),
-        take_picam_still_chained.s(
-            snap_id=snap_id,
-            group_id=group_id,
-            normal_exposure_pic_id=normal_exposure_picam_pic_id,
-            long_exposure_pic_id=long_exposure_picam_pic_id
-        ),
-        scale_image_chained.s(
-            img_id_in=thermal_pic_id,
-            img_id_out=scaled_pic_id
-        ),
-        merge_images_chained.s(
-            img1_primary_id_in=normal_exposure_picam_pic_id,
-            img1_alternate_id_in=long_exposure_picam_pic_id,
-            img2_id_in=scaled_pic_id,
-            img_id_out=merged_pic_id
-        )
-    ).apply_async(countdown=delay)
+    for i in [x*delay for x in range(1,repeat+2)]:
+        chain(
+            thermal_still_task.s(
+                snap_id=snap_id,
+                group_id=group_id,
+                pic_id=thermal_pic_id
+            ),
+            take_picam_still_chained.s(
+                snap_id=snap_id,
+                group_id=group_id,
+                normal_exposure_pic_id=normal_exposure_picam_pic_id,
+                long_exposure_pic_id=long_exposure_picam_pic_id
+            ),
+            scale_image_chained.s(
+                img_id_in=thermal_pic_id,
+                img_id_out=scaled_pic_id
+            ),
+            merge_images_chained.s(
+                img1_primary_id_in=normal_exposure_picam_pic_id,
+                img1_alternate_id_in=long_exposure_picam_pic_id,
+                img2_id_in=scaled_pic_id,
+                img_id_out=merged_pic_id
+            )
+        ).apply_async(countdown=i)
 
     return {
         'snap_id': str(snap_id),
--- camera/views.py
+++ camera/views.py
@@ -18,7 +18,8 @@ def picam_still():
     snap_id = uuid.uuid4()
     group_id = get_settings_document()['current_group_id']
     delay = get_delay_parameter()
-    ret_dict = take_picam_still(snap_id=snap_id, group_id=group_id, delay=delay)
+    repeat = get_repeat_parameter()
+    ret_dict = take_picam_still(snap_id=snap_id, group_id=group_id, delay=delay, repeat=repeat)
     return Response(json.dumps(ret_dict), status=202, mimetype='application/json')
 
 @camera.route('/thermal_still')
@@ -26,11 +27,9 @@ def thermal_still():
     snap_id = uuid.uuid4()
     group_id = get_settings_document()['current_group_id']
     delay = get_delay_parameter()
-    scale_image = True
-    if request.args.has_key('scale_image'):
-        scale_image = request.args.get('scale_image')
-        # need a cleaner way to parse a boolean from a get parameter.  e.g. 'False' evaluates to True as a non-empty string
-    ret_dict = take_thermal_still(snap_id=snap_id, group_id=group_id, delay=delay, scale_image=scale_image)
+    repeat = get_repeat_parameter()
+    scale_image = get_scale_image_parameter()
+    ret_dict = take_thermal_still(snap_id=snap_id, group_id=group_id, delay=delay, repeat=repeat, scale_image=scale_image)
     return Response(json.dumps(ret_dict), status=202, mimetype='application/json')
 
 @camera.route('/both_still')
@@ -38,11 +37,13 @@ def both_still():
     snap_id = uuid.uuid4()
     group_id = get_settings_document()['current_group_id']
     delay = get_delay_parameter()
+    repeat = get_repeat_parameter()
 
     both_still_dict = both_still_chain(
         snap_id=snap_id,
         group_id=group_id,
-        delay=delay
+        delay=delay,
+        repeat=repeat
     )
 
     return Response(json.dumps(both_still_dict), status=202, mimetype='application/json')
@@ -55,3 +56,19 @@ def get_delay_parameter():
     except ValueError as e:
         pass
     return delay
+
+def get_repeat_parameter():
+    repeat = 0
+    try:
+        if request.args.has_key('repeat'):
+            repeat = int(request.args.get('repeat'))
+    except ValueError as e:
+        pass
+    return repeat
+
+def get_scale_image_parameter():
+    scale_image = True
+    if request.args.has_key('scale_image'):
+        scale_image = request.args.get('scale_image')
+        # need a cleaner way to parse a boolean from a get parameter.  e.g. 'False' evaluates to True as a non-empty string
+    return scale_image
