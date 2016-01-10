c2bc5e3c19bed31d6fb1a2d621af8a30aef7a3a1
Dave Caulton <dcaulton@gmail.com>
2016-01-10 16:38:29 -0600

    Break up functionality in analysis.services a bit to improve testability

--- analysis/services.py
+++ analysis/services.py
@@ -6,10 +6,8 @@ from flask import current_app
 from PIL import Image, ImageStat, ImageOps
 
 from admin.services import get_group_document
+from picture.services import find_picture, save_picture_document
 from thermal.appmodule import celery
-
-def do_stuff():
-    return {'analysis stuff': 'just got done'}
 
 def check_if_image_is_too_dark(filename, brightness_threshold):
     image = Image.open(filename).convert('L')
@@ -24,6 +22,9 @@ def scale_image_chained(_, img_id_in, img_id_out):
     scale_image(img_id_in, img_id_out)
 
 @celery.task
+def scale_image_task(img_id_in, img_id_out):
+    scale_image(img_id_in, img_id_out)
+
 def scale_image(img_id_in, img_id_out):
 # only works on black and white images for now
     group_document = get_group_document('current')
@@ -31,11 +32,11 @@ def scale_image(img_id_in, img_id_out):
     if 'colorize_range_low' in group_document and 'colorize_range_high' in group_document:
         colorize_range_low = group_document['colorize_range_low']
         colorize_range_high = group_document['colorize_range_high']
-    img_dict_in = current_app.db[str(img_id_in)]
+    img_dict_in = find_picture(str(img_id_in))
     img_filename_in = img_dict_in['filename']
-    img_filename_out = "{0}.jpg".format(img_id_out)
-    pic_path_in = os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], img_filename_in)
-    pic_path_out = os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], img_filename_out)
+    img_filename_out = build_picture_name(img_id_out)
+    pic_path_in = build_picture_path(img_filename_in)
+    pic_path_out = build_picture_path(img_filename_out)
     image_in = Image.open(pic_path_in)
     image_scaled = image_in.resize(
                         (current_app.config['STILL_IMAGE_WIDTH'], current_app.config['STILL_IMAGE_HEIGHT']),
@@ -44,6 +45,7 @@ def scale_image(img_id_in, img_id_out):
     image_colorized = ImageOps.colorize(image_scaled, colorize_range_low, colorize_range_high)
     image_colorized.save(pic_path_out)
     img_dict_out = {
+        '_id': str(img_id_out),
         'type': 'picture',
         'source': 'analysis',
         'source_image_id': str(img_id_in),
@@ -54,4 +56,10 @@ def scale_image(img_id_in, img_id_out):
         'uri': "file://{0}{1}".format(current_app.config['HOSTNAME'], pic_path_out),
         'created': str(datetime.datetime.now())
     }
-    current_app.db[str(img_id_out)] = img_dict_out
+    save_picture_document(img_dict_out)
+
+def build_picture_name(picture_id):
+    return "{0}.jpg".format(picture_id)
+
+def build_picture_path(picture_name):
+    return os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], picture_name)
--- analysis/views.py
+++ analysis/views.py
@@ -1,14 +1,10 @@
 import json
 
 from flask import Blueprint, request, Response
-from analysis.services import do_stuff, scale_image
+
+from analysis.services import scale_image_task
 
 analysis = Blueprint('analysis', __name__)
-
-@analysis.route('/')
-def do_analysis_stuff():
-    things = do_stuff()
-    return Response(json.dumps(things), status=200, mimetype='application/json')
 
 @analysis.route('/scale_image')
 def call_scale_image():
@@ -18,5 +14,5 @@ def call_scale_image():
     if request.args.has_key('result_id'):
         result_id = request.args.get('result_id')
     if img_id_in and result_id:
-        scale_image.delay(img_id_in=img_id_in, img_id_out=result_id)
+        scale_image_task.delay(img_id_in=img_id_in, img_id_out=result_id)
         return Response(json.dumps('request accepted'), status=202, mimetype='application/json')
