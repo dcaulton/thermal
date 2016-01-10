0986ab1bc9ce774a7af6fb0b17f9c5a7d5203530
Dave Caulton <dcaulton@gmail.com>
2016-01-10 16:54:19 -0600

    Refactor merging.services a bit to make it more testable

--- merging/services.py
+++ merging/services.py
@@ -6,6 +6,7 @@ from flask import current_app
 from PIL import Image, ImageChops
 
 from admin.services import get_group_document
+from picture.services import find_picture, save_picture_document
 from thermal.appmodule import celery
 
 
@@ -36,20 +37,21 @@ def merge_images(img1_primary_id_in, img1_alternate_id_in, img2_id_in, img_id_ou
     else:
         merge_method = getattr(ImageChops, 'screen')
 
-    img1_dict_in = current_app.db[str(img1_id_in)]
+    img1_dict_in = find_picture(str(img1_id_in))
     img1_filename_in = img1_dict_in['filename']
-    img2_dict_in = current_app.db[str(img2_id_in)]
+    img2_dict_in = find_picture(str(img2_id_in))
     img2_filename_in = img2_dict_in['filename']
-    img_filename_out = "{0}.jpg".format(str(img_id_out))
-    pic1_path_in = os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], img1_filename_in)
-    pic2_path_in = os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], img2_filename_in)
-    pic_path_out = os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], img_filename_out)
+    img_filename_out = build_picture_name(img_id_out)
+    pic1_path_in = build_picture_path(img1_filename_in)
+    pic2_path_in = build_picture_path(img2_filename_in)
+    pic_path_out = build_picture_path(img_filename_out)
     image1_in = Image.open(pic1_path_in)
     image2_in = Image.open(pic2_path_in)
     image_out = merge_method(image1_in.convert('RGBA'), image2_in.convert('RGBA'))
     image_out.save(pic_path_out)
 
     img_dict_out = {
+        '_id': str(img_id_out),
         'type': 'picture',
         'source': 'merge',
         'source_image_id_1': str(img1_id_in),
@@ -61,4 +63,10 @@ def merge_images(img1_primary_id_in, img1_alternate_id_in, img2_id_in, img_id_ou
         'uri': "file://{0}{1}".format(current_app.config['HOSTNAME'], pic_path_out),
         'created': str(datetime.datetime.now())
     }
-    current_app.db[str(img_id_out)] = img_dict_out
+    save_picture_document(img_dict_out)
+
+def build_picture_name(picture_id):
+    return "{0}.jpg".format(picture_id)
+
+def build_picture_path(picture_name):
+    return os.path.join(current_app.config['PICTURE_SAVE_DIRECTORY'], picture_name)
