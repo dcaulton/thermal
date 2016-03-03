4601562670e474aca5c4e68f9aacc54ac0871787
Dave Caulton <dcaulton@gmail.com>
2016-03-03 17:31:32 -0600

    Add the endpoints for calibration.views

--- calibration/views.py
+++ calibration/views.py
@@ -19,3 +19,48 @@ def index():
 #        'scale_image': url_base + url_for('analysis.call_scale_image'),
     }
     return Response(json.dumps(top_level_links), status=200, mimetype='application/json')
+
+
+@calibration.route('/distortion_set')
+def get_distortion_set(methods=['GET']):
+    pass
+
+
+@calibration.route('/distortion_set')
+def update_distortion_set(methods=['PUT']):
+    pass
+
+
+@calibration.route('/distortion_set')
+def create_distortion_set(methods=['POST']):
+    pass
+
+
+@calibration.route('/distortion_pair')
+def get_distortion_pair(methods=['GET']):
+    pass
+
+
+@calibration.route('/distortion_pair')
+def update_distortion_pairt(methods=['PUT']):
+    pass
+
+
+@calibration.route('/distortion_pair')
+def create_distortion_pair(methods=['POST']):
+    pass
+
+
+@calibration.route('/calibration_session')
+def get_calibration_session(methods=['GET']):
+    pass
+
+
+@calibration.route('/calibration_session')
+def update_calibration_session(methods=['PUT']):
+    pass
+
+
+@calibration.route('/calibration_session')
+def create_calibration_session(methods=['POST']):
+    pass
