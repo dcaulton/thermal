46357471c70cf07b59df2ae993492efb6ebf94ca
Dave Caulton <dcaulton@gmail.com>
2016-03-05 10:14:15 -0600

    Measure test coverage when pytest_all is invoked

--- thermal/bin/pytest_all.sh
+++ thermal/bin/pytest_all.sh
@@ -1,8 +1,28 @@
 #! /bin/sh
 
-py.test admin
-py.test analysis
-py.test camera
-py.test merging
-py.test picture
-py.test thermal
+cd ~/thermal
+py.test --cov-report= \
+        --cov-config ./.coveragerc \
+        --cov=admin admin
+py.test --cov-report= \
+        --cov-config ./.coveragerc \
+        --cov-append \
+        --cov=analysis analysis
+py.test --cov-report= \
+        --cov-config ./.coveragerc \
+        --cov-append \
+        --cov=camera camera
+py.test --cov-report= \
+        --cov-config ./.coveragerc \
+        --cov-append \
+        --cov=merging merging
+py.test --cov-report= \
+        --cov-config ./.coveragerc \
+        --cov-append \
+        --cov=picture picture
+py.test --cov-report term-missing \
+        --cov-config ./.coveragerc \
+        --cov-append \
+        --cov=thermal thermal
+
+coverage report -m > coverage_report
